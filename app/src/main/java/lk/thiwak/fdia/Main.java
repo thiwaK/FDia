985cc79decd090f00180ef1db3d0a59b7aa6c939
Thiwanka Kaushal <thiwanka.kaushal.mob@gmail.com>
2026-01-13 10:17:59 +0530

    initial commit

---
+++ app/src/main/java/lk/thiwak/fdia/Main.java
@@ -0,0 +1,12 @@
+package lk.thiwak.fdia;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+
+public class Main implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
